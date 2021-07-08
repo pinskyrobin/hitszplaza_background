b0a5a06b81f4075967ab45d6ed1db562ead03d79
x568059888 <xu568059888@163.com>
2021-07-08 23:51:30 +0800

    新增对已写完的API的测试.

--- src/main/java/com/hitszplaza/background/controller/UserPosterController.java
+++ src/main/java/com/hitszplaza/background/controller/UserPosterController.java
@@ -7,15 +7,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/userPoster")
 public class UserPosterController {
     @Autowired
     private UserPosterServiceImpl userPosterService;
 
+    @PostMapping("/findAll")
+    public ArrayList<String> findAllUserPoster(@RequestParam Integer number) {
+        return userPosterService.findAll(number);
+    }
+
     @PostMapping("/update")
-    public String updatePosterStatus(@RequestParam String id,
+    public String updateUserPosterStatus(@RequestParam String id,
                                      @RequestParam Boolean valid) {
-        return userPosterService.updateStatus(id, valid);
+        return userPosterService.updateOneStatus(id, valid);
     }
 }
