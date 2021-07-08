a68063857c8e4d8d8a467625239c595e3a8569df
x568059888 <xu568059888@163.com>
2021-07-08 23:51:14 +0800

    新增若干API.

--- src/main/java/com/hitszplaza/background/service/UserPosterService.java
+++ src/main/java/com/hitszplaza/background/service/UserPosterService.java
@@ -1,6 +1,23 @@
 package com.hitszplaza.background.service;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public interface UserPosterService {
-    String findAll(Integer number);
-    String updateStatus(String id, Boolean valid);
+    ArrayList<String> findAll(Integer number);
+
+    String updateOneStatus(String id, Boolean valid);
+
+    //TODO:任务列表(优先度依次递减)
+    String deleteOne(String id);
+
+    String findAllWithOpenId(String openId);
+
+    String findAllWithType(String type);
+
+    String findAllWithTime(Date time);
+
+    String findAllWithAnoymous(Integer number);
+
+    String updateAllStatusWithOpenId(String openId);
 }
