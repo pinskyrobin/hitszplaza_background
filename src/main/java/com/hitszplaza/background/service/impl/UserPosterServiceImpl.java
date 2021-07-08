6b4ea5d7f9c1df03aed61e19e8416fda0084581f
x568059888 <xu568059888@163.com>
2021-07-08 23:53:12 +0800

    为查询操作提供分页处理的功能,所有页聚合到一个ArrayList数组中.

--- src/main/java/com/hitszplaza/background/service/impl/UserPosterServiceImpl.java
+++ src/main/java/com/hitszplaza/background/service/impl/UserPosterServiceImpl.java
@@ -8,6 +8,9 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 @Slf4j
 @Service
 public class UserPosterServiceImpl implements UserPosterService {
@@ -16,22 +19,36 @@ public class UserPosterServiceImpl implements UserPosterService {
     private WeChatUtil weChatUtil;
 
     @Override
-    public String findAll(Integer number) {
+    public ArrayList<String> findAll(Integer number) {
         // 请求地址
         String url = WeChatAPIConstant.WX_API_HOST +
                 "/tcb/databasequery?access_token=";
         // 数据库查询语句
-        String query = "db.collection(\"userPoster\")";
-        query = (number != null) ?
-                query.concat(String.format(".limit(%d).get()", number)) :
-                query.concat(".get()");
+        String pre_query = "db.collection(\"userPoster\")";
+        // 默认每次查询20条
+        if (number == null) {
+            number = 20;
+        }
+        int count = weChatUtil.getCount("userPoster");
+        int skipCount;
+        // 查询轮数
+        int batchTimes = (int) Math.ceil((double) count / number);
 
-        // 发送POST查询请求
-        return weChatUtil.post(url, query);
+        ArrayList<String> response = new ArrayList<>();
+        String raw_response;
+
+        for (int i = 0; i < batchTimes; i++) {
+            skipCount = i * number;
+            String query = pre_query + String.format(".skip(%d).limit(%d).get()", skipCount, number);
+            raw_response = weChatUtil.post(url, query);
+            //TODO:数据结果不够优雅,还需修改!
+            response.add(new JSONObject(raw_response).getJSONArray("data").toString());
+        }
+        return response;
     }
 
     @Override
-    public String updateStatus(String id, Boolean valid) {
+    public String updateOneStatus(String id, Boolean valid) {
         // 请求地址
         String url = WeChatAPIConstant.WX_API_HOST +
                 "/tcb/databaseupdate?access_token=";
@@ -48,6 +65,48 @@ public class UserPosterServiceImpl implements UserPosterService {
             log.warn("未修改任何记录");
         }
         return response;
+    }
+
+    @Override
+    public String deleteOne(String id) {
+        // 请求地址
+        String url = WeChatAPIConstant.WX_API_HOST +
+                "/tcb/databasedelete?access_token=";
+        // 数据库查询语句
+        String query = "db.collection(\"userPoster\")"
+                .concat(String.format(".doc(\"%s\").remove()", id));
+        String response =  weChatUtil.post(url, query);
+        JSONObject json = new JSONObject(response);
+        if ((Integer) json.get("deleted") == 0) {
+            log.warn("未删除任何记录!");
+        }
+        return response;
+    }
+
+    @Override
+    public String findAllWithOpenId(String openId) {
+        return null;
+    }
+
+    @Override
+    public String findAllWithType(String type) {
+        return null;
+    }
+
+    @Override
+    public String findAllWithTime(Date time) {
+        return null;
+    }
+
+    @Override
+    public String findAllWithAnoymous(Integer number) {
+        return null;
+    }
+
+    //TODO: remain to be completed!
+    @Override
+    public String updateAllStatusWithOpenId(String openId) {
+        return null;
     }
 
 
