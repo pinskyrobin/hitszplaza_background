9c70b9eeb3290e0b82b34b43f33ac962dd33a1ab
PinskyRobin <xu568059888@163.com>
2021-08-05 00:29:17 +0800

    解除对 MySQL 的加载限制。

--- src/main/java/com/hitszplaza/background/BackgroundApplication.java
+++ src/main/java/com/hitszplaza/background/BackgroundApplication.java
@@ -1,14 +1,17 @@
 package com.hitszplaza.background;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @EnableScheduling
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class BackgroundApplication {
 
 	public static void main(String[] args) {
