c9452e8113fb81495edcf72277319bfdb3458a4f
PinskyRobin <xu568059888@163.com>
2021-11-02 16:41:44 +0800

    写了一些通用接口,但是很可能并不会真正使用.

--- src/main/java/com/hitszplaza/background/controller/UniversalController.java
+++ src/main/java/com/hitszplaza/background/controller/UniversalController.java
@@ -5,8 +5,13 @@ import com.google.gson.JsonObject;
 import com.hitszplaza.background.constant.WeChatAPIConstant;
 import com.hitszplaza.background.constant.WeChatDataBaseQueryConstant;
 import com.hitszplaza.background.utils.WeChatUtil;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+/***
+ * 通用控制接口,用于处理一些通用的请求
+ */
 
 @RestController
 @RequestMapping("/")
@@ -22,7 +27,32 @@ public class UniversalController {
         String url = WeChatAPIConstant.WX_API_HOST + "/tcb/databaseaggregate?access_token=";
         System.out.println(query);
         String response = weChatUtil.post(url, query);
-        return new Gson().fromJson(response, JsonObject .class);
+        return new Gson().fromJson(response, JsonObject.class);
     }
+
+    @PatchMapping("/status")
+    public JsonObject updateStatus(@RequestParam String id,
+                                   @RequestParam Integer status,
+                                   @RequestParam String database) {
+        // 请求地址
+        String url = WeChatAPIConstant.WX_API_HOST +
+                "/tcb/databaseupdate?access_token=";
+        // 数据库查询语句
+        String query = String.format("db.collection(\"%s\").doc(\"%s\")" +
+                ".update({data:{status: %d}})", database, id, status);
+        return weChatUtil.update(url, query);
+    }
+
+    @DeleteMapping
+    public JsonObject delete(@RequestParam String id,
+                             @RequestParam String database) {
+        // 请求地址
+        String url = WeChatAPIConstant.WX_API_HOST +
+                "/tcb/databasedelete?access_token=";
+        // 数据库查询语句
+        String query = String.format("db.collection(\"%s\").doc(\"%s\").remove()", database, id);
+        return weChatUtil.delete(url, query);
+    }
+
 
 }
